cd5811fc5f4f6156b5bc51f9089226040a4c6392
yasuda282 <n2i.yasuda@gmail.com>
2020-03-10 14:54:12 +0900

    add-check-result-permission

--- app/src/main/java/com/example/androidapplication002/MainActivity.java
+++ app/src/main/java/com/example/androidapplication002/MainActivity.java
@@ -9,8 +9,11 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.telephony.TelephonyManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +37,42 @@ public class MainActivity extends AppCompatActivity {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            try {
-                Thread.sleep(3000); // 3秒間だけ処理を止める
-            } catch (InterruptedException ignored) {
-            }
-        }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            return;
+
         } else {
             TelephonyManager manager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
             String deviceId = manager.getDeviceId();
-            myWebView.loadUrl("https://stg.sapocha-sys-crew01.info/?crew_code=9001000002&shop_code=SAPOCHA01");
+            myWebView.loadUrl("https://stg.sapocha-sys-crew01.info/?crew_code=9001000002&shop_code=SAPOCHA01" + "&imei_number=" + deviceId);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "パーミッション追加しました", Toast.LENGTH_SHORT).show();
+
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    TelephonyManager manager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+                    String deviceId = manager.getDeviceId();
+                    myWebView.loadUrl("https://stg.sapocha-sys-crew01.info/?crew_code=9001000002&shop_code=SAPOCHA01"+"&imei_number="+deviceId);
+                } else {
+                    Toast.makeText(this, "パーミッション追加できませんでした", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
         }
     }
 }
